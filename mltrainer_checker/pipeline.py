dd337887fcd8fd17aa7a1a0e12fedcbaf76d5646
hiendang7613 <18120363@student.hcmus.edu.vn>
2023-09-16 19:56:02 +0700

    fafa

--- mltrainer_checker/pipeline.py
+++ mltrainer_checker/pipeline.py
@@ -16,15 +16,7 @@ def test_trainer(
     ):
   is_train = tf_train_loader or torch_train_loader
   is_test = tf_test_loader or torch_test_loader
-  W_Torch2TF(torch_model, tf_model)
-  print("Info: Copy weights TF2Torch passed.\n")
-  if torch.cuda.is_available():
-    torch_model = torch_model.to('cuda')
-
-  SetZeroLearningRate(torch_optimizer, tf_optimizer)
-  print("Info: SetZeroLearningRate done.\n")
-
-  # Prepare dataloader
+  # Check dataloader
   if torch_train_loader: 
     tf_train_loader = Torch_loader2TF_loader(torch_train_loader, batch_size, loader_length)
   if tf_train_loader: 
@@ -35,17 +27,26 @@ def test_trainer(
   if tf_test_loader: 
     torch_test_loader = TF_loader2Torch_loader(tf_test_loader, batch_size, loader_length)
     tf_test_loader = tf_test_loader.unbatch().batch(batch_size)
-  print("Info: Prepare dataloader done.\n")
+  print("Dataloader passed !!\n")
+  # Copy weights
+  W_Torch2TF(torch_model, tf_model)
+  print("Copy weights TF2Torch passed !!\n")
+  # To device
+  if torch.cuda.is_available():
+    torch_model.to('cuda')
+  # SetZeroLearningRate
+  SetZeroLearningRate(torch_optimizer, tf_optimizer)
+  print("SetZeroLearningRate passed !!\n")
   # tf_train_loader = tf_train_loader.unbatch().batch(batch_size)
   trainer_checker = TrainerChecker(
     torch_trainer, tf_trainer, 
     train_metrics, test_metrics,
     loader_length, batch_size
     )
-  if is_train:
-    if not trainer_checker.check_train_step(tf_train_loader, torch_train_loader):
-      return False
   if is_test:
     if not trainer_checker.chek_test_step(tf_test_loader, torch_test_loader):
+      return False
+  if is_train:
+    if not trainer_checker.check_train_step(tf_train_loader, torch_train_loader):
       return False
   return True
--- mltrainer_checker/trainer_checker.py
+++ mltrainer_checker/trainer_checker.py
@@ -1,90 +1,5 @@
 from tqdm import tqdm
-import torch
-from .copy_weights import W_Torch2TF
-import tensorflow as tf
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
-
-def Torch_loader2TF_loader(torch_dataloader, batch_size, length=10e30):
-  data = []
-  for i, x in enumerate(torch_dataloader):
-    data.append([tensor.numpy() for tensor in x])
-    if i >= length*batch_size:
-      break
-  data = [list(x) for x in zip(*data)]
-  return tf.data.Dataset.from_tensor_slices(tuple(data)).batch(batch_size)
-
-def TF_loader2Torch_loader(tf_dataloader, batch_size, length=10e30):
-  unbatch_tf_dataloader = tf_dataloader.unbatch()
-  data = []
-  for i, x in enumerate(unbatch_tf_dataloader):
-    data.append(tuple(tensor.numpy() for tensor in x))
-    if i >= length*batch_size:
-      break
-  return DataLoader(MyTorchDataset(data), batch_size=batch_size)
-
-
-def SetZeroLearningRate(torch_optimizer, tf_optimizer):
-  new_lr = 0.
-  for param_group in torch_optimizer.param_groups:
-    param_group['lr'] = new_lr
-  # tf_optimizer.learning_rate.assign(new_lr)
-  tf.keras.backend.set_value(tf_optimizer.learning_rate, new_lr)
-
-def test_trainer(
-    tf_trainer, torch_trainer, 
-    tf_model, torch_model,
-    tf_optimizer, torch_optimizer,
-    tf_train_loader = None, torch_train_loader = None,
-    tf_test_loader = None, torch_test_loader = None,
-    train_metrics = ['loss'], test_metrics = ['full'],
-    batch_size = 16, loader_length=10,
-    ):
-  is_train = tf_train_loader or torch_train_loader
-  is_test = tf_test_loader or torch_test_loader
-  # Check dataloader
-  if torch_train_loader: 
-    tf_train_loader = Torch_loader2TF_loader(torch_train_loader, batch_size, loader_length)
-  if tf_train_loader: 
-    torch_train_loader = TF_loader2Torch_loader(tf_train_loader, batch_size, loader_length)
-    tf_train_loader = tf_train_loader.unbatch().batch(batch_size)
-  if torch_test_loader: 
-    tf_test_loader = Torch_loader2TF_loader(torch_test_loader, batch_size, loader_length)
-  if tf_test_loader: 
-    torch_test_loader = TF_loader2Torch_loader(tf_test_loader, batch_size, loader_length)
-    tf_test_loader = tf_test_loader.unbatch().batch(batch_size)
-  print("Dataloader passed !!\n")
-  # Copy weights
-  W_Torch2TF(torch_model, tf_model)
-  print("Copy weights TF2Torch passed !!\n")
-  # To device
-  if torch.cuda.is_available():
-    torch_model.to('cuda')
-  # SetZeroLearningRate
-  SetZeroLearningRate(torch_optimizer, tf_optimizer)
-  print("SetZeroLearningRate passed !!\n")
-  # tf_train_loader = tf_train_loader.unbatch().batch(batch_size)
-  trainer_checker = TrainerChecker(
-    torch_trainer, tf_trainer, 
-    train_metrics, test_metrics,
-    loader_length, batch_size
-    )
-  if is_test:
-    if not trainer_checker.chek_test_step(tf_test_loader, torch_test_loader):
-      return False
-  if is_train:
-    if not trainer_checker.check_train_step(tf_train_loader, torch_train_loader):
-      return False
-  return True
-
-class MyTorchDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
-    def __len__(self):
-        return len(self.data)
-    def __getitem__(self, idx):
-        return self.data[idx]
-
 
 class TrainerChecker:
     def __init__(self, torch_trainer, tf_trainer,
@@ -108,7 +23,7 @@ class TrainerChecker:
             if not np.isclose(torch_metrics[metric_name], tf_metrics[metric_name]):
               print("Error: Different train_step !!\n")
               return False
-        print("Check train_step passed !!\n")
+        print("Info: Check train_step passed !!\n")
         return True
 
     def chek_test_step(self, tf_dataloader, torch_dataloader):  
@@ -123,5 +38,5 @@ class TrainerChecker:
               print("Error: Different test_step !!\n")
               print(metric_name, torch_metrics[metric_name], tf_metrics[metric_name])
               return False
-        print("\nCheck test_step passed !!\n")
+        print("Info: Check test_step passed !!\n")
         return True
